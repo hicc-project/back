431dfdf6610798215be83f4ac7960b0f6594dbd9
minsu0701v <kangmins3@naver.com>
2026-01-26 14:49:14 +0900

    fix db

--- backend/settings.py
+++ backend/settings.py
@@ -93,7 +93,7 @@ import dj_database_url
 
 DATABASES = {
     "default": dj_database_url.config(
-        default=os.environ.get("DATABASE_URL", f"sqlite:///{BASE_DIR / 'db.sqlite3'}"),
+        default=os.environ.get("DATABASE_URL"),
         conn_max_age=600,
     )
 }
